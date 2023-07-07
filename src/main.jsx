0d0fd0fb233f4bebf60063181a5ca9c134eb5d4c
Daniel Camilo <daniel.camilo@ez4.com.br>
2023-07-07 20:41:16 -0300

    Project Weather React Application.

---
+++ src/main.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+function App() {
+  const [data, setData] = useState("");
+
+  const getData = async () => {
+    let location = document.getElementById("city").value;
+    let API_KEY = "7d9eeb8ace209da0d28e33e2dd6d0fcf";
+
+    let response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${location}&lang=pt_br&appid=${API_KEY}&units=metric`
+    );
+    let data = await response.json();
+
+    console.log(data);
+    setData(data);
+  };
+
+  return (
+    <>
+      <div className="wrapper">
+        <div className="content">
+          <h2>Qual cidade você quer ver agora?</h2>
+          <div className="searchBox">
+            <input
+              type="text"
+              name=""
+              id="city"
+              placeholder="Busque por uma cidade"
+            />
+            <button id="btn" onClick={getData}>
+              Buscar
+            </button>
+          </div>
+        </div>
+        {data && (
+          <div className="city-info">
+            <div className="row cityInfo">
+              <span id="cityName">{data.name}</span>
+              <span id="temp">{data.main.temp}</span>
+            </div>
+            <div className="row">
+              <span id="status">{data.weather[0].description}</span>
+              <span id="media">{data.name}</span>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
